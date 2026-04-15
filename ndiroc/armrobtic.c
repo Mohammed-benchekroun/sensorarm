7b371616c81be30bdd862ddcf6738ef69d4cf05d
Mohammed-benchekroun <benchekrounlakrimim@gmail.com>
2026-04-15 14:15:09 +0100

    just one printf

--- ndiroc/armrobtic.c
+++ ndiroc/armrobtic.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 int main() {
 	printf("hello mohammedSSSSsqsqSS first second change");
-	printf("hello mohammedSSSSsqsqSS first second change");
-	printf("hello mohammedSSSSsqsqSS first second chansge");
-	printf("hello mohammedSSSSsqsqSS first second change");
+
 } 
