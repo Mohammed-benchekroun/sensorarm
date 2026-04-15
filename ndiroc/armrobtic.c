bc5ea9d3580a4b8beae4bebe483c7c9fed17b7e9
Mohammed-Benchekroun <benchekrounlakrimim@gmail.com>
2026-04-15 14:13:31 +0100

    thred test

--- ndiroc/armrobtic.c
+++ ndiroc/armrobtic.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
 int main() {
 	printf("hello mohammedSSSSsqsqSS first second change");
+	printf("hello mohammedSSSSsqsqSS first second change");
+	printf("hello mohammedSSSSsqsqSS first second chansge");
+	printf("hello mohammedSSSSsqsqSS first second change");
 } 
